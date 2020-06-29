f43320604c83ff3433f3b53b72c293aa94b0a804
Willyan Antunes <willyan.dantunes@gmail.com>
2020-06-28 23:24:08 -0300

    🐛 sending coordinates to i.a

--- app/Controllers/Http/GetScenesController.js
+++ app/Controllers/Http/GetScenesController.js
@@ -3,32 +3,32 @@
 const axios = use('axios');
 
 class GetScenesController {
-  index({request}) {
-    const {
+  async index({ request }) {
+    const { cloudCover, dateEnd, dateInit, listIds, coords } = request.post();
+    const value = this.normalizeScenes({
       cloudCover,
       dateEnd,
       dateInit,
       listIds,
-    } = request.post();
-    const cloud = cloudCover
-    const endDate = dateEnd
-    const initDate = dateInit
-    const onId = listIds
-    const value = this.normalizeScenes({cloud, endDate, initDate, onId})
-    const { data } = await axios.post(`${process.env.IA_ENDPOINT}/generate-mask`, value);
-    
-    return {message:'ok'}
+      coords,
+    });
+    const { data } = await axios.post(
+      `${process.env.IA_ENDPOINT}/generate-mask`,
+      value
+    );
+
+    return { message: 'ok', ...data };
   }
 
-  normalizeScenes({cloud, endDate, initDate, onId}) {
-    return{
-      dateInit: new Date(initDate),
-      dateEnd: new Date(endDate),
-      cloudCover: cloud,
-      ids: onId,
+  normalizeScenes({ cloudCover, dateEnd, dateInit, listIds, coords }) {
+    return {
+      dateInit: new Date(dateInit),
+      dateEnd: new Date(dateEnd),
+      cloudCover,
+      ids: listIds,
+      coordinates: coords,
     };
   }
-
 }
 
 module.exports = GetScenesController;
